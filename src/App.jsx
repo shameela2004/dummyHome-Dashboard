fe075adbbd0d82eb0e4b1f6c9b5fa07bc2d9d4c1
shameela2004 <fathimashameela2004@gmail.com>
2025-07-26 16:35:10 +0530

    next

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import Login from './login'
+
 import {Route, Routes} from "react-router-dom"
 import Home from './components/Home'
+import Login from './Login'
 import ManageProducts from './components/ManageProducts'
 import Products from './components/Products'
 
